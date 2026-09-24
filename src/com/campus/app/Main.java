2ad4eadeeae37e3ca334e40c0ef99ee036284604
syed2006-hub <110123104109@aalimec.ac.in>
2026-09-24 11:37:24 +0530

    hald of day 4

--- src/com/campus/app/Main.java
+++ src/com/campus/app/Main.java
@@ -1,7 +1,56 @@
 package com.campus.app;
 
+import com.campus.model.Student;
+import com.campus.service.StudentService;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Campus Management System");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter student ID");
+        int studentId = sc.nextInt();
+        sc.nextLine(); 
+
+        System.out.println("Enter student name");
+        String studentName = sc.nextLine();
+
+        System.out.println("Enter student Age");
+        int studentAge = sc.nextInt();
+        sc.nextLine(); 
+
+        System.out.println("Enter student Department");
+        String studentDept = sc.nextLine();
+
+        System.out.println("Enter Number of subjects");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the marks for " + n + " subjects");
+
+        int[] marks = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the mark of subject " + (i + 1));
+            marks[i] = sc.nextInt();
+        }
+
+        Student student = new Student(
+                studentId,
+                studentName,
+                studentAge,
+                studentDept,
+                marks
+        );
+
+        student.displayStudentInfo(true);
+
+        Student.displayStudentCount();
+
+        StudentService ss = new StudentService();
+        ss.displayReportCard(student);
+
+        sc.close();
     }
 }
